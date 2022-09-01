2eff285dc5e01188c7d011b551c1777d7d9db3f8
hellogreenriver <maqatibet@hotmail.co.jp>
2022-09-01 22:57:32 +0900

    first commit

---
+++ src/main/java/com/example/stockprophet/StockprophetApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockprophet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockprophetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockprophetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stockprophet/StockprophetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockprophet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockprophetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
