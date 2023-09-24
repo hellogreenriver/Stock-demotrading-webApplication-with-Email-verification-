1f5a81acd9ba96671ed980ff80fdb278339934e2
hellogreenriver <65156255+tetramin1955@users.noreply.github.com>
2023-09-24 20:10:17 +0900

    modify java file

--- src/main/java/com/example/stockprophet/config/SecurityConfig.java
+++ src/main/java/com/example/stockprophet/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
         corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
         corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
         corsConfiguration.addExposedHeader("X-AUTH-TOKEN");
-        corsConfiguration.addAllowedOrigin("https://puffy-ocean-production.up.railway.app");
+        corsConfiguration.addAllowedOrigin("https://stock-chart-5f639d359786.herokuapp.com");
         corsConfiguration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
         corsSource.registerCorsConfiguration("/**", corsConfiguration);
